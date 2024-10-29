cae2b72417493a40c9364a10e59343c7edc98adc
Cleo Silva <cleo_silva1906@hotmail.com>
2024-10-29 16:54:28 -0300

    Inserted decorators

--- src/main/java/edu/api/todolist/model/Task.java
+++ src/main/java/edu/api/todolist/model/Task.java
@@ -1,6 +1,20 @@
 package edu.api.todolist.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private boolean completed;
